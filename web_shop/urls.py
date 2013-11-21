f20c3072635369ddad96a544eb94eade82a4de42
Matthias <matthias@eckhart-onlinemedien.com>
2013-11-21 19:49:26 +0100

    - Added Logfile and ER-Model - aligned URL structure

--- web_shop/urls.py
+++ web_shop/urls.py
@@ -3,7 +3,7 @@ from shopit.settings import MEDIA_ROOT
 
 urlpatterns = patterns('',
     url(r'^$', 'web_shop.views.welcome'),
-    url(r'^add_product$', 'web_shop.views.add_product'),
+    url(r'^market/add_product$', 'web_shop.views.add_product'),
     url(r'^market/$', 'web_shop.views.market'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT, 'show_indexes':True}),
 )
