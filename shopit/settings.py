f0ae3134b7ca1ee0fa89556d63711031753a7a46
Nenad <nenad_savovic@yahoo.com>
2013-11-23 15:29:01 +0100

    active_link.js externalized for web_shop/base.html and accounts/profile.html

--- shopit/settings.py
+++ shopit/settings.py
@@ -59,14 +59,15 @@ WSGI_APPLICATION = 'shopit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shopit',
-        'USER': 'Matthias',
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlite3'),
+        'USER': '',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '',
         'PORT': '',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
