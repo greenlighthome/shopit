c9ced67405721b7ebad59005975f3d2ee8634a0d
Nenad <nenad_savovic@yahoo.com>
2013-12-06 23:03:05 +0100

    tooltip added

--- shopit/settings.py
+++ shopit/settings.py
@@ -67,15 +67,14 @@ WSGI_APPLICATION = 'shopit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shopit',
-        'USER': 'postgres',
-        'PASSWORD': 'neubaugasse99',
-        'HOST': 'localhost',
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
